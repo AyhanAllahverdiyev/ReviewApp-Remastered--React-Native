0e69019b7dc6cad3689ba179b7eafcb4f2d8a67a
aykhanallahverdizade <20290882@ogrenci.ankara.edu.tr>
2024-03-26 23:52:38 +0300

    images for ratings

--- screens/home.js
+++ screens/home.js
@@ -1,24 +1,51 @@
-import React,{useState} from 'react';
-import { View, Text, Button,TouchableOpacity,FlatList} from 'react-native';
+// import React,{useState} from 'react';
+// import { View, Text, Button,TouchableOpacity,FlatList} from 'react-native';
+// import { globalStyles } from '../styles/global';
+//  import Card from '../shared/card';
+//   const Home = ({navigation}) => {
+
+//   const [reviews, setReviews] = useState([
+//       { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+//       { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+//       { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+//     ]);  
+
+//     return (
+//         <View style={globalStyles.container}>
+//         <FlatList data={reviews} renderItem={({ item }) => (
+//           <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails',{title:item.title,body:item.body,rating:item.rating,key:item.key,})}>
+//             <Card> <Text style={globalStyles.titleText}>{ item.title }</Text></Card>
+//           </TouchableOpacity>
+//         )} />
+//       </View>
+//     );
+// }
+
+// export default Home;
+
+
+
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
- 
-  const Home = ({navigation}) => {
+import Card from '../shared/card';
 
+export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
-      { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
-      { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
-      { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
-    ]);  
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ]);
 
-    return (
-        <View style={globalStyles.container}>
-        <FlatList data={reviews} renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails',{title:item.title,body:item.body,rating:item.rating,key:item.key,})}>
+  return (
+    <View style={globalStyles.container}>
+      <FlatList data={reviews} renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+          <Card>
             <Text style={globalStyles.titleText}>{ item.title }</Text>
-          </TouchableOpacity>
-        )} />
-      </View>
-    );
+          </Card>
+        </TouchableOpacity>
+      )} />
+    </View>
+  );
 }
-
-export default Home;
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { globalStyles } from '../styles/global';
-const ReviewDetails = ({route,navigation}) => {
+import { View, Text, Button,Image ,StyleSheet} from 'react-native';
+import { globalStyles,imgaes } from '../styles/global';
+import Card from '../shared/card';
+ const ReviewDetails = ({route,navigation}) => {
     const {title,body,rating,key} = route.params;
      return (
         <View style={globalStyles.container}>
+            <Card> 
             <Text style={globalStyles.titleText}>{ title }</Text>
             <Text style={globalStyles.titleText}>{ body }</Text>
-            <Text style={globalStyles.titleText}>{ rating }</Text>
-            <Button title='Back to Home' onPress={() => navigation.goBack()} />
+            <View style={styles.rating} >
+                  <Text>GameZone rating:</Text>
+                  <Image source={(imgaes.ratings[rating])}/>
+            </View>
+             <Button title='Back to Home' onPress={() => navigation.goBack()} />
+            </Card>
         
       </View>
        
     );
 }
-
+const styles=StyleSheet.create({
+    rating:{
+        flexDirection:'row',
+        justifyContent:'center',
+        paddingTop:16,
+        marginTop:16,
+        borderTopWidth:1,
+        borderTopColor:'#eee'
+    }
+    
+})
 export default ReviewDetails;
