a615fd3488a50e0f7a5c89d53889c3175cfe3058
aykhanallahverdizade <20290882@ogrenci.ankara.edu.tr>
2024-03-27 17:26:42 +0300

    About page

--- screens/about.js
+++ screens/about.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text,ImageBackground} from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function About() {
   return (
+    <ImageBackground source={require('../assets/game_bg.png')} style={styles.imageBackground}>
     <View style={globalStyles.container}>
-      <Text>About Screen</Text>
+      <View style={{justifyContent:'center', alignItems:'center'}}>       
+       </View>
+      <Text style={styles.aboutText}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus justo sed ex lobortis vehicula. Fusce vel felis non dolor pellentesque rutrum eget et ipsum. Nulla vel nisi eu elit pulvinar rutrum. Mauris a tellus non urna suscipit facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam ac quam urna. Pellentesque placerat risus ut nunc sodales, id vestibulum lectus tristique. 
+      </Text>
     </View>
+    </ImageBackground>
   );
 }
+
+const styles= StyleSheet.create( {
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  aboutText: {
+    marginHorizontal:20,
+    marginVertical:30,
+    fontSize:20,
+    fontWeight:'500',
+    color:'teal'
+  }
+})
--- screens/home.js
+++ screens/home.js
@@ -1,70 +1,120 @@
 import React, { useMemo, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, ImageBackground, StyleSheet ,Modal,SafeAreaView,Keyboard,TouchableWithoutFeedback} from 'react-native';
+import { View, Text,Alert, TouchableOpacity, FlatList, ImageBackground, StyleSheet, Modal, SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
-import {MaterialIcons} from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import ReviewForm from './reviewForm';
 
 export default function Home({ navigation }) {
-  const [modalOpen, setModelOpen]=useState(false);
+  const [modalOpen, setModelOpen] = useState(false);
   const [reviews, setReviews] = useState([
-    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
-    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
-    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1', showDelete: false },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2', showDelete: false },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3', showDelete: false },
   ]);
 
-  const addReview= (review)=>{
-    review.key=Math.random().toString(); 
-    setReviews((currentReviews)=>{
+  const toggleDeleteIcon = (key) => {
+    setReviews(reviews.map(review => {
+      if (review.key === key) {
+        return { ...review, showDelete: !review.showDelete };
+      }
+      return review;
+    }));
+  };
+
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    review.showDelete = false;
+    setReviews((currentReviews) => {
       return [review, ...currentReviews]
     });
     setModelOpen(false);
+  }
+  const askUser = (item) => {
+    Alert.alert(
+      "Confirmation",  
+      "Are you sure you want to delete?", 
+      [
+        {
+          text: 'Delete',
+          onPress: () => handleDelete(item),
+          style: 'destructive'
+        },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancelled'),
+          style: 'cancel'
+        }
+        
+      ],
+      { cancelable: false } 
+    );
+  }
+  
+  const handleDelete= (item)=>{
+    const filteredReviews = reviews.filter(review => review.key !== item.key);
+    setReviews(filteredReviews);
 
   }
+
   return (
-        <ImageBackground source={require('../assets/game_bg.png')} style={styles.imageBackground}>
-        <View style={styles.content}>
-          
-          <Modal visible={modalOpen} animationType='slide'> 
-              <View style={{height:50}}></View>
-              <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-              <View style={styles.modalContent}> 
-              
-              <MaterialIcons 
-                style={{...styles.modalToggle,...styles.modalClose }}
-                  name ="close" 
-                  size={24} 
-                  onPress={()=>setModelOpen(false)}
-                />          
-                  <View style={{height:20}}></View>
-                  <ReviewForm addReview={addReview}/>
-                </View>       
-              </TouchableWithoutFeedback>
-           </Modal>       
-          <MaterialIcons 
-       
+    <ImageBackground source={require('../assets/game_bg.png')} style={styles.imageBackground}>
+      <View style={styles.content}>
+
+        <Modal visible={modalOpen} animationType='slide'>
+          <View style={{ height: 50 }}></View>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.modalContent}>
+
+              <MaterialIcons
+                style={{ ...styles.modalToggle, ...styles.modalClose }}
+                name="close"
+                size={24}
+                onPress={() => setModelOpen(false)}
+              />
+              <View style={{ height: 20 }}></View>
+              <ReviewForm addReview={addReview} />
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
+        <MaterialIcons
           style={styles.modalToggle}
-              name ="add" 
-              size={24} 
-              onPress={()=>setModelOpen(true)}
-          />
-          <FlatList 
-            data={reviews} 
-            renderItem={({ item }) => (
-              <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-                <Card>
-                  <Text style={globalStyles.titleText}>{item.title}</Text>
-                </Card>
-              </TouchableOpacity>
-            )} 
-          />
-        </View>
-      </ImageBackground>
-   );
+          name="add"
+          size={24}
+          onPress={() => setModelOpen(true)}
+        />
+        <FlatList
+          data={reviews}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onLongPress={() => toggleDeleteIcon(item.key)}
+              onPress={() => navigation.navigate('ReviewDetails', item)}>
+              <Card>
+                <View style={styles.deleteIcon}> 
+                <Text style={globalStyles.titleText}>{item.title}</Text>
+                {item.showDelete && (
+                  <MaterialIcons
+                    style={{ ...styles.modalToggle, ...styles.modalClose }}
+                    name="delete"
+                    color={'red'}
+                    size={24}
+                    onPress={() => {
+                      askUser(item)
+                    }}
+                  />
+                )}
+                </View>
+              </Card>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
- 
+
   imageBackground: {
     flex: 1,
     resizeMode: 'cover',
@@ -74,23 +124,28 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     width: '100%',
-    padding: 20, 
+    padding: 20,
   },
-  modalToggle:{
-    marginBottom:10,
-    borderWidth:1,
-    borderColor:'gray',
-     padding:10,
-    borderRadius:10,
-    alignSelf:'center',
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
   },
-  modalClose:{
-    marginTop:20,
-    marginBottom:0,
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
   },
-  modalContent:{
+  modalContent: {
+    flex: 1,
+    padding: 20
+  },
+  deleteIcon:{
+    justifyContent:'space-between',
     flex:1,
-    padding:20
+    flexDirection:'row'
   }
-   
+
 });
